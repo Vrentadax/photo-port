92e98bdea5d4a3c9571ada9f911b2406333ff6f1
Vrentadax <vrentadax@outlook.com>
2022-06-13 13:11:02 -0600

    finished nav functions and tests

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -6,20 +6,24 @@ import Nav from '..';
 
 const categories = [
     { name: 'portraits', description: 'Portraits of people in my life' }
-]
-const mockCurrentCategory = jest.fn();
-const mockSetCurrentCategory = jest.fn();
+  ]
+  const mockCurrentCategory = jest.fn();
+  const mockSetCurrentCategory = jest.fn();
+  const mockContactSelected = jest.fn();
+  const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
 describe('Nav component', () => {
     it('renders', () => {
         render(<Nav
-            categories={categories}
-            setCurrentCategory={mockSetCurrentCategory}
-            currentCategory={mockCurrentCategory}
+          categories={categories}
+          setCurrentCategory={mockSetCurrentCategory}
+          currentCategory={mockCurrentCategory}
+          contactSelected={mockContactSelected}
+          setContactSelected={mockSetContactSelected}
         />);
-    });
+      })
 
     it('matches snapshot', () => {
         const { asFragment } = render(<Nav
